7fdde79232c231e14cd292583ce4e15ecf414691
Cody Allen <cwallen1221@gmail.com>
2025-08-27 10:12:30 -0500

    Add forward and back controls to W and S keys in player.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,12 +14,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+
         player.update(dt)
 
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
--- player.py
+++ player.py
@@ -2,11 +2,12 @@ import pygame
 from constants import *
 from circleshape import CircleShape
 
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-    
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
@@ -16,11 +17,15 @@ class Player(CircleShape):
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
-        return [a, b, c]    
-    
+        return [a, b, c]
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -28,3 +33,7 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
